daa922bcf7547cf54b9a2d4ca1cee62e58c365c5
chuck <charlesrymal@gmail.com>
2014-11-10 19:41:48 -0800

    use python makefile

--- make.py
+++ make.py
@@ -2,6 +2,16 @@
 
 import os
 import fnmatch
+import subprocess
+
+import render
+
+def mkdir(dirname):
+
+	try:
+		os.makedirs(dirname)
+	except OSError as err:
+		pass
 
 def glob(pat, directory = '.'):
 	for root, dirnames, filenames in os.walk('src'):
@@ -9,7 +19,65 @@ def glob(pat, directory = '.'):
 			yield os.path.join(root, filename)
 
 
+def subst(strings, pat1, pat2):
+	for s in strings:
+		yield s.replace(pat1, pat2)
+		
 
-print list(glob('*.md'))
+# gather files
+
+md_files = list(glob('*.md'))
+dot_files = list(glob('*.dot'))
+
+html_files = list(subst(md_files, '.md', '.html'))
+png_files = list(subst(dot_files, '.dot', '.png'))
+
+
+head1 = "<head><link href=\"http://kevinburke.bitbucket.org/markdowncss/markdown.css\" rel=\"stylesheet\"></link>"
+head2 = "<script type=\"text/javascript\"src=\"http://cdn.mathjax.org/mathjax/latest/MathJax.js?config=TeX-AMS-MML_HTMLorMML\"></script></head>"
+
+
+print md_files
+print html_files
+
+for d,p in zip(dot_files, png_files):
+	
+	p = 'build/' + p
+
+	dirname,_ = os.path.split(p)
+	mkdir(dirname)
+
+	
+
+	print 'd:', d
+	print 'p:', p
+	print 'dirname:', dirname
+
+	call = ["dot", d, "-Tpng", "-o" + p]
+
+	print 'call:', call
+
+	subprocess.call(call)
+
+for m,h in zip(md_files, html_files):
+
+	h = 'build/' + h
+
+	dirname,_ = os.path.split(h)
+	mkdir(dirname)
+	
+	with open(h, 'w') as f:
+		f.write(head1)
+		f.write(head2)
+
+	render.render(m,h)
+
+	#call = ['python', 'render.py ' + m + ' ' + h]
+	
+	#subprocess.call(call)
+	
+	
+
+
 
 
--- render.py
+++ render.py
@@ -4,55 +4,62 @@ import re
 import os
 import jinja2
 
-parser = argparse.ArgumentParser()
-parser.add_argument('md_file')
-parser.add_argument('html_file')
 
-args = parser.parse_args()
+def render(md_file, html_file):
+	# read md file
+	
+	with open(md_file, 'r') as f:
+		md_str = f.read()
+	
+	#md_dir = os.path.dirname(args.md_file)
+	#m = re.findall('\[.*?\]\((.*?)\)',md_text)
+	#for s in m:
+	#	src = os.path.join(md_dir, s)
+	#	print repr(src)
+	
+	# separate sidebar from center
+	
+	pat = re.compile('^&&&$', flags=re.MULTILINE)
+	
+	m = pat.search(md_str)
+	
+	if m:
+		md_sidebar_str = md_str[:m.start(0)]
+		md_center_str = md_str[m.end(0):]
+	else:
+		md_sidebar_str = 'sidebar'
+		md_center_str = md_str
+	
+	html_sidebar_str = markdown.markdown(md_sidebar_str, extensions=['extra', 'tables'])
+	html_center_str = markdown.markdown(md_center_str, extensions=['extra', 'tables'])
+	
+	# read template string from file
+	
+	h,_ = os.path.split(__file__)
 
-# read md file
+	with open(os.path.join(h, 'template.html'), 'r') as f:
+		temp_str = f.read();
+	
+	template = jinja2.Template(temp_str)
+	
+	# render template
+	
+	html_str = template.render(sidebar_str=html_sidebar_str, center_str=html_center_str)
+	
+	# write html file
+	
+	with open(html_file, 'w') as f:
+		f.write(html_str)
+	
+	
+if __name__ == '__main__':
 
-with open(args.md_file, 'r') as f:
-	md_str = f.read()
-
-#md_dir = os.path.dirname(args.md_file)
-#m = re.findall('\[.*?\]\((.*?)\)',md_text)
-#for s in m:
-#	src = os.path.join(md_dir, s)
-#	print repr(src)
-
-
-# separate sidebar from center
-
-pat = re.compile('^&&&$', flags=re.MULTILINE)
-
-m = pat.search(md_str)
-
-if m:
-	md_sidebar_str = md_str[:m.start(0)]
-	md_center_str = md_str[m.end(0):]
-else:
-	md_sidebar_str = 'sidebar'
-	md_center_str = md_str
-
-html_sidebar_str = markdown.markdown(md_sidebar_str, extensions=['extra', 'tables'])
-html_center_str = markdown.markdown(md_center_str, extensions=['extra', 'tables'])
-
-# read template string from file
-
-with open('template.html', 'r') as f:
-	temp_str = f.read();
-
-template = jinja2.Template(temp_str)
-
-# render template
-
-html_str = template.render(sidebar_str=html_sidebar_str, center_str=html_center_str)
-
-# write html file
-
-with open(args.html_file, 'w') as f:
-	f.write(html_str)
-
+	parser = argparse.ArgumentParser()
+	parser.add_argument('md_file')
+	parser.add_argument('html_file')
+	
+	args = parser.parse_args()
+	
+	render(args.md_file, args.html_file)
 
 
